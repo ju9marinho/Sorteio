30b9bda43e5338599f0b2ec41a1d8af53df4adb1
ju9marinho <ju9marinho@gmail.com>
2024-09-04 11:00:33 -0300

    operador de comparação

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
+    if(min >= max) {
+        alert("O valor mínimo tem que ser menor que o valor máximo")
+    } else {
+        (min < max)
+
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     alert(result) //pop up com o resultado
-
+    }
 }
